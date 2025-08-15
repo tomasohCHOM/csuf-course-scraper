b941a4973c7699d858b2a7528d27b8763efd7602
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-08-15 15:33:12 -0700

    working on scraping logic

--- cmd/api/search.go
+++ cmd/api/search.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 const BASE_URL = "https://catalog.fullerton.edu"
 
+type Course struct {
+	Title         string
+	Description   string
+	Prerequisites []string
+	Corequisites  []string
+}
+
+func GetCourse(url string) Course {
+	c := colly.NewCollector()
+	course := Course{}
+
+	c.OnHTML("td.block_content", func(e *colly.HTMLElement) {
+		title := strings.TrimSpace(e.DOM.Find("h1#course_preview_title").Text())
+		course.Title = title
+		courseContent := strings.Split(e.Text, "\n")
+		fmt.Println(courseContent)
+	})
+
+	c.Visit(url)
+	return course
+}
+
 func SearchCourses(courseQuery string) error {
 	url := fmt.Sprintf("%s/search_advanced.php?cur_cat_oid=80&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=3&filter[keyword]=%s&filter[exact_match]=1", BASE_URL, courseQuery)
-	fmt.Println(url)
 	c := colly.NewCollector()
 	// Scrape all the courses based on the CSS selector
 	c.OnHTML(`a[aria-expanded="false"]`, func(e *colly.HTMLElement) {
-		fmt.Println(e)
+		courseUrl := fmt.Sprintf("%s/%s", BASE_URL, e.Attr("href"))
+		GetCourse(courseUrl)
 	})
 	err := c.Visit(url)
 	if err != nil {
-		return fmt.Errorf("Failed to visit URL: %w", err)
+		return fmt.Errorf("failed to visit URL: %w", err)
 	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,19 +3,21 @@ module csuf-course-scraper
 go 1.22.3
 
 require (
+	github.com/gocolly/colly v1.2.0
+	github.com/labstack/echo/v4 v4.12.0
+)
+
+require (
 	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/antchfx/htmlquery v1.3.1 // indirect
 	github.com/antchfx/xmlquery v1.4.0 // indirect
 	github.com/antchfx/xpath v1.3.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
